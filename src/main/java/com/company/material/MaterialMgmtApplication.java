f837a6472fdf7e703d1996ec615f2a07d17f79cc
zhanglei <czxy408@163.com>
2026-06-11 11:35:31 +0800

    init: enterprise material management system base

---
+++ src/main/java/com/company/material/MaterialMgmtApplication.java
@@ -0,0 +1,11 @@
+package com.company.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialMgmtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialMgmtApplication.class, args);
+    }
+}
